eb77dbc44f38b026dd4a8d963fde277fadb7f5fd
gautam yadav <gy77044@gmail.com>
2024-03-30 08:12:37 +0530

    promise

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 // Async Javascript
 
 console.log("first");
-setTimeout(function () { console.log("Third"); }, 2000);
+setTimeout(function () { console.log("Third"); }, 0);
 console.log("Second");
+
+
+const ans = new Promise((res, rej) => {
+  var n = Math.floor(Math.random() * 10);
+
+  if (n < 5) {
+    return res()
+  } else {
+    return rej()
+  }
+})
+
+
+ans.then(
+  function () { console.log("below"); }
+)
+  .catch(
+    function () { console.log("above"); }
+  )
